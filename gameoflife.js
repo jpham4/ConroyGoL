972c74ab1393a7b585c35208767d8edc384955ea
Jimmy Pham <jpham21@outlook.com>
2017-10-02 01:52:39 -0400

    updated Grid, added CSS, and changed html layout

--- gameoflife.js
+++ gameoflife.js
@@ -1,20 +1,56 @@
-var rows, columns;
 
 function tablemake()
 {
-	
-		document.write("<table border ='4'>");
+    var rows, columns;
+        table.id = "table";
 		rows = prompt("Please enter the number of rows","");
 		columns = prompt("Please enter the number of columns","");
-		
-		for(j = 1; j <= rows; j++)
+
+		for(var j = 1; j <= rows; j++)
 			{
-				document.write("<tr>");
-				for(i = 1; i <= columns; i++)
+				var tr = document.createElement("tr");
+				for(var i = 1; i <= columns; i++)
 				{
-					document.write("<td></td>");
+					var td = document.createElement("td");
+					td.class = "tdClass1";
+
+                    td.onmousedown = function () {
+                        if (this.className != "tdClass2") {
+                            this.className = "tdClass2";
+                        }
+                        else {
+                            this.removeAttribute("Class");
+                        }
+                    };
+                    tr.appendChild(td);
+					table.appendChild(tr);
 				}
-				document.write("</tr>");
 			}
-		document.write("</table>");
  }
+
+ function Grid(grid,arr)
+ {
+     for(var i=0; i < grid.childNodes.length; ++i)
+     {
+         for(var j=0; j < grid.childNodes[i].childNodes.length; ++j)
+         {
+             grid.childNodes[i].childNodes[j].className = arr[i][j] ? 'tdClass2':'tdClass1';
+         }
+     }
+ }
+
+ function arrayMake(grid)
+ {
+     var array = [];
+     for(var i=0; i<grid.childNodes.length; ++i)
+     {
+         var array2 = [];
+         for(var j=0; j<grid.childNodes[i].childNodes.length; ++j)
+         {
+             array2[j] = modGrid(grid.childNodes[i].childNodes[j].className);
+         }
+         array[i] = array2;
+     }
+ }
+
+
